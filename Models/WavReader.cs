c5f0ff06a72f19ef31716212023d43244b7e9900
blonial <bloonial@gmail.com>
2020-05-31 15:12:01 +0200

    Reading WAV files

---
+++ Models/WavReader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace poid.Models
+{
+    public static class WavReader
+    {
+        public static bool Read(string filename, out float[] L, out float[] R)
+        {
+            L = R = null;
+            try
+            {
+                using (FileStream fs = File.Open(filename, FileMode.Open))
+                {
+                    BinaryReader reader = new BinaryReader(fs);
+
+                    int chunkID = reader.ReadInt32();
+                    int fileSize = reader.ReadInt32();
+                    int riffType = reader.ReadInt32();
+
+                    int fmtID = reader.ReadInt32();
+                    int fmtSize = reader.ReadInt32();
+                    int fmtCode = reader.ReadInt16();
+                    int channels = reader.ReadInt16();
+                    int sampleRate = reader.ReadInt32();
+                    int byteRate = reader.ReadInt32();
+                    int fmtBlockAlign = reader.ReadInt16();
+                    int bitDepth = reader.ReadInt16();
+
+                    if (fmtSize == 18)
+                    {
+                        int fmtExtraSize = reader.ReadInt16();
+                        reader.ReadBytes(fmtExtraSize);
+                    }
+
+                    int dataID = reader.ReadInt32();
+                    int bytes = reader.ReadInt32();
+
+                    byte[] byteArray = reader.ReadBytes(bytes);
+
+                    int bytesForSamp = bitDepth / 8;
+                    int samps = bytes / bytesForSamp;
+
+
+                    float[] asFloat = null;
+                    switch (bitDepth)
+                    {
+                        case 64:
+                            double[]
+                            asDouble = new double[samps];
+                            Buffer.BlockCopy(byteArray, 0, asDouble, 0, bytes);
+                            asFloat = Array.ConvertAll(asDouble, e => (float)e);
+                            break;
+                        case 32:
+                            asFloat = new float[samps];
+                            Buffer.BlockCopy(byteArray, 0, asFloat, 0, bytes);
+                            break;
+                        case 16:
+                            Int16[]
+                            asInt16 = new Int16[samps];
+                            Buffer.BlockCopy(byteArray, 0, asInt16, 0, bytes);
+                            asFloat = Array.ConvertAll(asInt16, e => e / (float)Int16.MaxValue);
+                            break;
+                        default:
+                            return false;
+                    }
+
+                    switch (channels)
+                    {
+                        case 1:
+                            L = asFloat;
+                            R = null;
+                            return true;
+                        case 2:
+                            L = new float[samps];
+                            R = new float[samps];
+                            for (int i = 0, s = 0; i < samps; i++)
+                            {
+                                L[i] = asFloat[s++];
+                                R[i] = asFloat[s++];
+                            }
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,134 @@
-﻿using poid.Models;
+﻿using Microsoft.Win32;
+using poid.Commands;
+using poid.Models;
+using System.Windows.Input;
 
 namespace poid.ViewModels
 {
     public class MainViewModel : NotifyPropertyChangedEvent
     {
+        #region Properties
 
+        private float[] _R;
+        public float[] R
+        {
+            get
+            {
+                return _R;
+            }
+            private set
+            {
+                _R = value;
+                NotifyPropertyChanged("R");
+            }
+        }
+
+        private float[] _L;
+        public float[] L
+        {
+            get
+            {
+                return _L;
+            }
+            private set
+            {
+                _L = value;
+                NotifyPropertyChanged("L");
+            }
+        }
+
+        private MonoType? _FileMonoType;
+        public MonoType? FileMonoType
+        {
+            get
+            {
+                return _FileMonoType;
+            }
+            private set
+            {
+                _FileMonoType = value;
+                NotifyPropertyChanged("FileMonoType");
+            }
+        }
+
+        private string _FileName;
+        public string FileName
+        {
+            get
+            {
+                return _FileName;
+            }
+            private set
+            {
+                _FileName = value;
+                NotifyPropertyChanged("FileName");
+            }
+        }
+
+        #endregion
+
+        #region Enums
+
+        public enum MonoType { Mono, Stereo }
+
+        #endregion
+
+        #region Constuctors
+
+        public MainViewModel()
+        {
+            this.InitializeCommands();
+        }
+
+        #endregion
+
+        #region Initializers
+
+        private void InitializeCommands()
+        {
+            this._LoadFile = new RelayCommand(this.LoadFile);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand _LoadFile { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        private void LoadFile(object o)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Browse WAV Files";
+            openFileDialog.DefaultExt = "wav";
+            openFileDialog.Filter = "WAV files (*.wav)|*.wav";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                float[] r;
+                float[] l;
+                if (WavReader.Read(openFileDialog.FileName, out l, out r))
+                {
+                    this.R = r;
+                    this.L = l;
+                    this.FileMonoType = r == null ? MonoType.Mono : MonoType.Stereo;
+                    this.FileName = openFileDialog.FileName;
+                    Notify.Info("Image loaded successfully!");
+                }
+                else
+                {
+                    Notify.Info("Failed to load file!");
+                    this.R = null;
+                    this.L = null;
+                    this.FileMonoType = null;
+                    this.FileName = null;
+                }
+            }
+        }
+
+        #endregion
     }
 }
