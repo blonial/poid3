92ded94980d14ab2b6f69d0c3fcc27cec5662d47
blonial <bloonial@gmail.com>
2020-05-31 16:40:28 +0200

    Base FFT module

---
+++ Models/AMath.cs
@@ -0,0 +1,40 @@
+﻿using AForge.Math;
+using System;
+
+namespace poid.Models
+{
+    public static class AMath
+    {
+        #region Static Methods
+
+        public static Complex[] FFT(float[] channel)
+        {
+            Complex[] data = new Complex[channel.Length];
+
+            for (int i = 0; i < channel.Length; i++)
+            {
+                data[i] = new Complex(channel[i], 0);
+            }
+
+            FourierTransform.FFT(data, FourierTransform.Direction.Forward);
+
+            return data;
+        }
+
+        public static float[] IFFT(Complex[] data)
+        {
+            FourierTransform.FFT(data, FourierTransform.Direction.Backward);
+
+            float[] result = new float[data.Length];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Convert.ToSingle(-data[i].Re);
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
